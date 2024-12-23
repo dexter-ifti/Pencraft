decc8d8acab8b2e5dcb864e3b9886b8e1aa15cb9
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2024-12-23 22:10:47 +0530

    feat: add index file to export page components

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,9 @@
+import Blog from './Blog';
+import Landing from './Landing';
+import Login from './Login';
+import Signup from './Signup';
+import WriteBlog from './WriteBlog';
+
+export {
+    Blog, Landing, Login, Signup, WriteBlog
+}
